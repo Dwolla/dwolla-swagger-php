9439982b062b7eb83463e385ced41a130fcf27ee
Chris Piggott <chris@homebase.ai>
2019-02-27 14:57:13 -0600

    Update PHPDoc for BusinessclassificationApi.php and add properties

--- lib/BusinessclassificationsApi.php
+++ lib/BusinessclassificationsApi.php
@@ -24,6 +24,16 @@ namespace DwollaSwagger;
 
 class BusinessclassificationsApi {
 
+  /**
+   * @var ApiClient $apiClient instance of the ApiClient
+   */
+  private $apiClient;
+
+  /**
+   * @var array $authSettings array of authentication methods
+   */
+  public $authSettings;
+
   function __construct($apiClient = null) {
     if (null === $apiClient) {
       if (Configuration::$apiClient === null) {
@@ -40,17 +50,20 @@ class BusinessclassificationsApi {
     $this->authSettings = array('oauth2');
   }
 
-  private $apiClient; // instance of the ApiClient
 
   /**
-   * get the API client
+   * Returns an instance of the API Client
+   *
+   * @return ApiClient|null
    */
   public function getApiClient() {
     return $this->apiClient;
   }
 
   /**
-   * set the API client
+   * Set the API client
+   *
+   * @param ApiClient $apiClient
    */
   public function setApiClient($apiClient) {
     $this->apiClient = $apiClient;
@@ -62,7 +75,8 @@ class BusinessclassificationsApi {
    *
    * Get a list business classifications.
    *
-   * @return BusinessClassificationListResponse
+   * @return models\BusinessClassificationListResponse
+   * @throws ApiException
    */
    public function _list() {
       
@@ -113,7 +127,8 @@ class BusinessclassificationsApi {
    * Get a business classification with a list of industry classifications.
    *
    * @param string $id Id of business classification to get. (required)
-   * @return BusinessClassification
+   * @return models\BusinessClassification
+   * @throws ApiException
    */
    public function getBusinessClassification($id) {
       
