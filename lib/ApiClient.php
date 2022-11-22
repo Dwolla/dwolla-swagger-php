30d0811aef57acdf68656a332ce36da9b869a46b
James Coon <james@jcoon.dev>
2022-11-22 02:37:15 -0500

    DEV-1167: Bump version to 1.8.0 (#59)

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -35,7 +35,7 @@ class ApiClient {
   /*
    * @var string user agent of the HTTP request, set to "PHP-Swagger" by default
    */
-  protected $user_agent = "php-swagger-1.7.1";
+  protected $user_agent = "php-swagger-1.8.0";
 
   /**
    * @param string $host Base url of the API server (optional)
